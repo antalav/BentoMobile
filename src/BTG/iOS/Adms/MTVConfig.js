c3fd8437240f2025dea6d9567ee95e03fcdcf9d1
antalav <veantala@gmail.com>
2014-11-19 17:00:06 -0500

    no message

---
+++ src/BTG/iOS/Adms/MTVConfig.js
@@ -0,0 +1,38 @@
+[
+	{
+	
+	"appId" : "mtv_app",
+	"applicationName" : "Mtv iOS App",
+	"ariaEnabled" : true,
+	"nielsenEnabled" : true,
+	"nielsenAppID" : "T383342AD-ACC2-4BFA-86A1-2CA4ECB932F6",
+	"nielsenSFCode" : "uat-cert",
+	"loggingEnabled" : true,
+	"admsMeasureEnabled" : true,
+	"admsSslEnabled" : false,
+	"admsDebugLoggingEnabled" : true,
+	"admsTimeSpentTrackingEnabled" : false,
+	"admsOfflineTrackingEnabled" : false,
+	"audienceManagerEnabled" : true,
+	"admsLifecycleSessionTimeout" : 300,
+	"admsOfflineHitLimit" : 1000,
+	"admsReportSuiteIDs" : "'viamtvapp'",
+	"admsTrackingServer" : "sc.mtv.com",
+	"admsVisitorID" : "",
+	"admsCharacterSet" : "ISO-8859-1",
+	"admsCurrencyCode" : "USD",
+	"comScoreEnabled" : false,
+	"comScorePrimaryID" : "6036034",
+	"comScorePublisherSecret" : "674879b3e61bb0264a49c40226ed442b",
+	"demdexEnabled" : true,
+	"keychainAccessGroup" : "",
+	"loggingEnabled" : true,
+	"tveEnabled" : false,
+	"isCoppaCompliant" : false,
+		"admsContextData"   : {
+			"contextDataKey1onconfig"    : "testcontextvalue1onconfig",
+			"contextDataKey2onconfig"    : "testcontextvalue2onconfig",
+			"contextDataKey1trackpage"    : "testcontextvalue1onconfig"
+		}
+	}
+]
