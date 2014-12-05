ec71fc05e8b7ea465268cb16f9d6810c6657e634
antalav <veantala@gmail.com>
2014-12-04 20:48:28 -0500

    no message

---
+++ src/BTG/Android/Omniture/tveconfig.js
@@ -0,0 +1,17 @@
+[
+	{
+	omnitureEnabled: "true",
+	omnitureSuite: "viarnd",
+	omnitureChannel: "Bento TestRig Channel",
+	omnitureCharSet: "ISO-8859-1",
+	omnitureCurrencyCode: "USD",
+	omnitureSSL: "false",
+	omnitureLifecycleSessionTimeout: "300",
+	omnitureTrackingServer: "sc.mtv.com",
+	omnitureVisitorID: "",
+	omnitureDebugLogging: "true",
+	demdexEnabled: "true",
+	TVEEnabled: "true",
+	comScoreEnabled : "true"
+	}
+]
