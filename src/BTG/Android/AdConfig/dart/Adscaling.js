6d5a3669598ba91ee8a4a8bf2047c2d8ccd3affc
antalav <veantala@gmail.com>
2014-12-17 10:25:23 -0500

    adding ad scale config

---
+++ src/BTG/Android/AdConfig/dart/Adscaling.js
@@ -0,0 +1,22 @@
+[
+	{
+		doubleclickKeyValues: "",
+		doubleclickAdSize: "300x250",
+		doubleclickIsDfp: false,
+		doubleclickStaticPixelSize: "575x400",
+		doubleclickDfpData: {
+			metadata: {
+				property: "nick",
+				is_main: false
+			},
+			mappings: {
+				ad-unit: {
+				 "@property",
+ 				 "shows",
+				 "victorious",
+ 				 "home"
+				 }
+				}
+			}
+	}
+]
