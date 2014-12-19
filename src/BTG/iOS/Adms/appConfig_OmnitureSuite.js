5eab81cb2710d5e5132e3a2063daf86afdc6bf71
antalav <veantala@gmail.com>
2014-12-19 14:44:16 -0500

    Update appConfig_OmnitureSuite.js

--- src/BTG/iOS/Adms/appConfig_OmnitureSuite.js
+++ src/BTG/iOS/Adms/appConfig_OmnitureSuite.js
@@ -7,7 +7,7 @@
 		"admsSslEnabled" : true,
 		"admsLifecycleSessionTimeout" : 300,
 		"admsTrackingServer" : "Test.mtv.com",
-		"admsVisitorID" : "",
+		"admsVisitorID" : "Test Visitor ID",
 		"admsDebugLoggingEnabled" : false,
 		"admsOfflineTrackingEnabled" : false,
 		"audienceManagerEnabled" : true,
